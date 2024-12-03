fa6f461a22d61008fc479b0a8cba60cfc0763fa0
Arriva <92034212+AMB05@users.noreply.github.com>
2024-12-03 11:17:27 +0700

    Delete data_transaksi directory

--- data_transaksi/models/sale_order.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields  
-
-class SaleOrder(models.Model):  
-    _inherit = 'sale.order'  
-
-    transaction_id = fields.Many2one(
-        'account.transaction', 
-        string='Data Transaction', 
-        tracking=True)
